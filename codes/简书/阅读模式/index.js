2634455b396571f8cefc8f6831f31008494598f0
sogrey <suzhaoheng2006@163.com>
2021-09-16 00:13:28 +0800

    add 简书/阅读模式

---
+++ codes/简书/阅读模式/index.js
@@ -0,0 +1,46 @@
+/**
+ * 简书 (https://www.jianshu.com/) 文章页阅读模式
+ * 
+ * https://www.jianshu.com/p/{pID}
+ * 
+ * 示例页面：
+ *   https://www.jianshu.com/p/cac06311888c
+ */
+
+
+/** tools */
+var getElement = function (eleId) {
+    return document.querySelector(eleId);
+}
+var getElementAll = function (eleId) {
+    return document.querySelectorAll(eleId);
+}
+var displayNoneElements = function (eles) {
+    if (eles && eles.length > 0) {
+        eles.forEach(item => item.style.display = 'none');
+    }
+}
+
+String.prototype.startWith = function (endStr) {
+    var d = this.length - endStr.length;
+    return (endStr.length > 0 && d >= 0 && this.indexOf(endStr) == 0)
+}
+
+/** main */
+
+var jianshuHost = "https://www.jianshu.com/p/{pID}";
+
+var regJianshuHost = /https:\/\/www\.jianshu\.com\/p\//g;
+
+if (regJianshuHost.test(document.location.href)) { // 简书
+    // 隐藏section 保留第一个
+    displayNoneElements(getElementAll('section'));
+    var firstSection = getElement('section');
+    firstSection.style.display = "block";
+    firstSection.style.width = "100%";
+
+    // 隐藏右侧
+    displayNoneElements(getElementAll('aside'));
+} else {
+    alert(`此插件仅支持 ${jianshuHost}`);
+}
