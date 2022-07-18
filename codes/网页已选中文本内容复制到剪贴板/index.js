5c06cc233ab32059bee92cd7d832a3e169f0c16f
Sogrey <suzhaoheng2006@163.com>
2022-07-18 14:19:12 +0800

    deploy main

--- codes/网页已选中文本内容复制到剪贴板/index.js
+++ codes/网页已选中文本内容复制到剪贴板/index.js
@@ -37,9 +37,28 @@ var getSelectionText = function () {
     return text;
 }
 
-function copyToClipBoard(text) {
-    window.clipboardData.setData("Text", text);
-    alert("复制成功");
+function copyToClipBoard(_sTxt) {
+    try {
+        if (window.clipboardData) {
+            window.clipboardData.setData("Text", _sTxt);
+        } else if (window.netscape) {
+            netscape.security.PrivilegeManager.enablePrivilege('UniversalXPConnect');
+            var clip = Components.classes['@mozilla.org/widget/clipboard;1'].createInstance(Components.interfaces.nsIClipboard);
+            if (!clip) return;
+            var trans = Components.classes['@mozilla.org/widget/transferable;1'].createInstance(Components.interfaces.nsITransferable);
+            if (!trans) return;
+            trans.addDataFlavor('text/unicode');
+            var str = new Object();
+            var len = new Object();
+            var str = Components.classes["@mozilla.org/supports-string;1"].createInstance(Components.interfaces.nsISupportsString);
+            var copytext = _sTxt;
+            str.data = copytext;
+            trans.setTransferData("text/unicode", str, copytext.length * 2);
+            var clipid = Components.interfaces.nsIClipboard;
+            if (!clip) return false;
+            clip.setData(trans, null, clipid.kGlobalClipboard);
+        }
+    } catch (e) { console.error(e); }
 }
 
 var run = function () {
